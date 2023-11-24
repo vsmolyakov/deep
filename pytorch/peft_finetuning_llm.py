d8ce8ee57d893bc9842fb3e36fdb71d31f6133f2
Vadim Smolyakov <vsmolyakov@gmail.com>
2023-11-23 20:40:38 -0500

    peft finetuning
    peft finetuning

---
+++ pytorch/peft_finetuning_llm.py
@@ -0,0 +1,180 @@
+from datasets import load_dataset
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer
+from peft import LoraConfig, get_peft_model, TaskType
+from peft import PeftModel, PeftConfig
+
+import torch
+import time
+import evaluate
+import pandas as pd
+import numpy as np
+
+def print_number_of_trainable_model_parameters(model):
+    trainable_model_params = 0
+    all_model_params = 0
+    for _, param in model.named_parameters():
+        all_model_params += param.numel()
+        if param.requires_grad:
+            trainable_model_params += param.numel()
+    return f"trainable model parameters: {trainable_model_params}\nall model parameters: {all_model_params}\npercentage of trainable model parameters: {100 * trainable_model_params / all_model_params:.2f}%"
+
+def tokenize_function(example):
+    start_prompt = 'Summarize the following conversation.\n\n'
+    end_prompt = '\n\nSummary: '
+    prompt = [start_prompt + dialogue + end_prompt for dialogue in example["dialogue"]]
+    example['input_ids'] = tokenizer(prompt, padding="max_length", truncation=True, return_tensors="pt").input_ids
+    example['labels'] = tokenizer(example["summary"], padding="max_length", truncation=True, return_tensors="pt").input_ids
+    
+    return example
+
+huggingface_dataset_name = "knkarthick/dialogsum"
+dataset = load_dataset(huggingface_dataset_name)
+#dataset
+
+model_name='google/flan-t5-base'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+print(print_number_of_trainable_model_parameters(original_model))
+
+index = 200
+dialogue = dataset['test'][index]['dialogue']
+human_baseline_summary = dataset['test'][index]['summary']
+
+prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary:
+"""
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+
+dash_line = '-'.join('' for x in range(100))
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{human_baseline_summary}')
+print(dash_line)
+print(f'ORIGINAL MODEL - ZERO SHOT:\n{original_model_text_output}')
+
+# The dataset actually contains 3 diff splits: train, validation, test.
+# The tokenize_function code is handling all data across all splits in batches.
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+tokenized_datasets = tokenized_datasets.remove_columns(['id', 'topic', 'dialogue', 'summary',])
+
+#downsample the data
+tokenized_datasets = tokenized_datasets.filter(lambda example, index: index % 100 == 0, with_indices=True)
+print(f"Shapes of the datasets:")
+print(f"Training: {tokenized_datasets['train'].shape}")
+print(f"Validation: {tokenized_datasets['validation'].shape}")
+print(f"Test: {tokenized_datasets['test'].shape}")
+
+print(tokenized_datasets)
+
+lora_config = LoraConfig(
+    r=32, # Rank
+    lora_alpha=32,
+    target_modules=["q", "v"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type=TaskType.SEQ_2_SEQ_LM # FLAN-T5
+)
+
+peft_model = get_peft_model(original_model, lora_config)
+print(print_number_of_trainable_model_parameters(peft_model))
+
+output_dir = f'./data/peft-finetuning-{str(int(time.time()))}'
+
+peft_training_args = TrainingArguments(
+    output_dir=output_dir,
+    auto_find_batch_size=True,
+    learning_rate=1e-3, # Higher learning rate than full fine-tuning.
+    num_train_epochs=1,
+    logging_steps=1,
+    max_steps=1    
+)
+    
+peft_trainer = Trainer(
+    model=peft_model,
+    args=peft_training_args,
+    train_dataset=tokenized_datasets["train"],
+)
+
+peft_trainer.train()
+
+peft_model_path="./peft-checkpoint"
+peft_trainer.model.save_pretrained(peft_model_path)
+tokenizer.save_pretrained(peft_model_path)
+
+peft_model_base = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base", torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+
+peft_model = PeftModel.from_pretrained(peft_model_base, 
+                                       './peft-checkpoint/', 
+                                       torch_dtype=torch.bfloat16,
+                                       is_trainable=False)
+
+print(print_number_of_trainable_model_parameters(peft_model))
+
+index = 200
+dialogue = dataset['test'][index]['dialogue']
+baseline_human_summary = dataset['test'][index]['summary']
+
+prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary: """
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+peft_model_outputs = peft_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+peft_model_text_output = tokenizer.decode(peft_model_outputs[0], skip_special_tokens=True)
+
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{human_baseline_summary}')
+print(dash_line)
+print(f'PEFT MODEL:\n{peft_model_text_output}')
+
+dialogues = dataset['test'][0:10]['dialogue']
+human_baseline_summaries = dataset['test'][0:10]['summary']
+
+original_model_summaries = []
+peft_model_summaries = []
+
+for idx, dialogue in enumerate(dialogues):
+    prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary: """
+    
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    human_baseline_text_output = human_baseline_summaries[idx]
+    
+    peft_model_outputs = peft_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    peft_model_text_output = tokenizer.decode(peft_model_outputs[0], skip_special_tokens=True)
+
+    peft_model_summaries.append(peft_model_text_output)
+
+zipped_summaries = list(zip(human_baseline_summaries, peft_model_summaries))
+ 
+df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'peft_model_summaries'])
+
+rouge = evaluate.load('rouge')
+
+peft_model_results = rouge.compute(
+    predictions=peft_model_summaries,
+    references=human_baseline_summaries[0:len(peft_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('PEFT MODEL:')
+print(peft_model_results)
